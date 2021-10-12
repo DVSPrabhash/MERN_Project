307ac47513a3391660d0189fc836b7280db089b2
ZephyrClint <it20148768@my.sliit.lk>
2021-10-12 07:14:13 +0530

    thiran_search3_correction

--- frontend/src/components/inventory_manager/Search3.js
+++ frontend/src/components/inventory_manager/Search3.js
@@ -21,7 +21,7 @@ const Search3 = ({history}) => {
                 <input
                     type="text"
                     id="inventory_search_field"
-                    placeholder="Enter keyword..."
+                    placeholder="Enter supplier ID..."
                     onChange={(e) => setKeyword(e.target.value)}
                     className="searchBox_Thiran"
                 />
